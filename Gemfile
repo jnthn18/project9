d62902009fe63166f99f05fcfaf4e776c3e3d375
jnthn18 <jnthn18@gmail.com>
2015-02-09 11:58:56 -0500

    Updated to use sinatra-contrib

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source 'https://rubygems.org'
 ruby '2.1.5'
 
 gem 'sinatra', '~> 1.4.5'
+gem 'rack', '1.5.2'
+gem 'sinatra-contrib', '1.4.2'
--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 # app.rb
 require 'sinatra'
+require 'sinatra/content_for'
 
 class MyWebApp < Sinatra::Base
+	helpers Sinatra::ContentFor
 	get '/' do
 		erb :"index#{ rand(3) + 1 }"
 	end
