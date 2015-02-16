f6be802c1c5e009e02d27e9ac50d8d9ad263cc7f
jnthn18 <jnthn18@gmail.com>
2015-02-16 12:01:42 -0500

    Cleaned up code from last project

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
-# Gemfile
 source 'https://rubygems.org'
 ruby '2.1.5'
 
 gem 'sinatra', '~> 1.4.5'
 gem 'rack', '1.5.2'
 gem 'sinatra-contrib', '1.4.2'
+gem 'holidapi'
--- app.rb
+++ app.rb
@@ -1,10 +1,14 @@
-# app.rb
 require 'sinatra'
 require 'sinatra/content_for'
+require 'holidapi'
 
 class MyWebApp < Sinatra::Base
 	helpers Sinatra::ContentFor
+	time = Time.new
+
 	get '/' do
-		erb :"index#{ rand(3) + 1 }"
+		@holidays = HolidApi.get(country: 'us', year: time.year, month: time.month)
+		@holidays_bday = HolidApi.get(country: 'us', year: 1992, month: 1)
+		erb :index
 	end
 end
