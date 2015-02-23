f9e72dc1c4fe5479b70bba6e08d91b49c2421a27
jnthn18 <jnthn18@gmail.com>
2015-02-23 11:37:49 -0500

    inital commit

--- app.rb
+++ app.rb
@@ -4,11 +4,10 @@ require 'holidapi'
 
 class MyWebApp < Sinatra::Base
 	helpers Sinatra::ContentFor
-	time = Time.new
 
 	get '/' do
-		@holidays = HolidApi.get(country: 'us', year: time.year, month: time.month)
-		@holidays_bday = HolidApi.get(country: 'us', year: 1992, month: 1)
+		puts params
+		@holidays = HolidApi.get(params)
 		erb :index
 	end
 end
