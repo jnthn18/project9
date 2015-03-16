139776d312e6b6673b13833800b9a0d5bfb20636
jnthn18 <jnthn18@gmail.com>
2015-03-16 11:40:30 -0400

    removed old code

--- spec/spec_helper.rb
+++
@@ -1,6 +0,0 @@
-require_relative '../app'
-
-MethodsPracticeApp.environment = :test
-Bundler.require :default, MethodsPracticeApp.environment
-
-require_relative 'support/helpers/methods_practice_helpers_spec_helpers'
