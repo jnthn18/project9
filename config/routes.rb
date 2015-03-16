7a1785cb7a74ae649e58e8fbcdb14335f435794f
jnthn18 <jnthn18@gmail.com>
2015-03-16 12:01:54 -0400

    Added sqlite3 to development

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   root 'static#home'
 
   get '/about' => 'static#about'
-  get '/cat-pictures'                 => 'static#cats'
-  get '/cat-pictures/:number_of_cats' => 'static#cats'
-  #get '/cat-pictures(/:number_of_cats)' => 'static#cats'
+  get '/cat-pictures(/:number_of_cats)' => 'static#cats'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
