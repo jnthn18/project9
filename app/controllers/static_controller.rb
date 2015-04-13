3b48904d9747f1c0e5967c1e337d3d152cf6ee95
jnthn18 <jnthn18@gmail.com>
2015-04-12 20:10:35 -0400

    replaced hard tabs with soft tabs

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,6 +6,6 @@ class StaticController < ApplicationController
   end
 
   def cats
-  	@cats = CatAPI.new.get_images(results_per_page: params[:number_of_cats] || 1)
+    @cats = CatAPI.new.get_images(results_per_page: params[:number_of_cats] || 1)
   end
 end
